0a9f0b2a4678e2cf7d1bf9d5cff642d89f8358b9
AlexMollohan <ajmollohan@gmail.com>
2017-06-10 19:17:13 -0400

    unable to get create field to render for some reason. Investigating

--- app/components/create-account-form.js
+++ app/components/create-account-form.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'form',
+  classNames: ['form-horizontal'],
+
+  params : {},
+
+  actions:{
+    submit () {
+      console.log(this);
+    },
+  }
 });
