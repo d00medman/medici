8e218ea971c75985b200c0bae3d593273360cf82
AlexMollohan <ajmollohan@gmail.com>
2017-06-11 00:40:35 -0400

    merging to main account feature branch

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -3,18 +3,19 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model () {
-    console.log(this.get('store').findRecord('account', 13));
-    return [{
-        id: 13,
-        userName : 'BLACKBART',
-        creator: false,
-        contentType: 'nil',
-        coinbaseEmail: 'borzaini@gmail.com'
-      }];
+    let targ = this.get('store').findRecord('account', 10);
+    console.log(targ.user_name);
+    // return [{
+    //     id: 13,
+    //     userName : 'BLACKBART',
+    //     creator: false,
+    //     contentType: 'nil',
+    //     coinbaseEmail: 'borzaini@gmail.com'
+    //   }];
     // let targ = this.get('store').findRecord('account', {user_id:12});
     // console.log(targ);
     // return targ;
-    //  return this.get('store').findAll('account');
+    return targ
   },
 
 });
