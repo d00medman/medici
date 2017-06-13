b5c13259031a9dfef8f41551c8bffd58a12118a0
AlexMollohan <ajmollohan@gmail.com>
2017-06-13 19:00:51 -0400

    Working on updating account name, switching branches to test possibility of single get functionality

--- app/routes/accounts/my-account.js
+++ app/routes/accounts/my-account.js
@@ -5,34 +5,7 @@ export default Ember.Route.extend({
   auth: Ember.inject.service(),
 
   model () {
-    // console.log(params);
     let currentUser = this.get('auth.credentials.id');
-    // let user = this.store.peekRecord('user', currentUser);
-    // user.get('account').then((account) => {
-    //   console.log(account);
-    // });
-    // console.log('current user ', currentUser);
-    // const adapter = this.store.adapterFor('account');
-    // let targ = adapter.getAccountId(currentUser);
-    // targ.then((accountId) => {
-    //   return this.get('store').findRecord('account', accountId);
-    // });
-    // return adapter.getAccountId(currentUser);
-    // let targ = (this.store.queryRecord('account', { filter: { user: { id:currentUser } } }));
-    // let targ = (this.store.queryRecord('account', { filter: { user_id: currentUser} }));
-    // console.log('targ: ', targ);
-    // targ = targ.id;
-    // This needs to be tremendously simplified and hardcoded
-    // console.log(targ);
-    // console.log(params);
-    // let user = this.store.peekRecord('user', currentUser);
-    // return this.store.queryRecord('account', { filter: { user: user} });
-    // console.log(targ.user_name);
-    // return targ;
-    // return this.get('store').findRecord('account', targ);
-    // let accountID = this.store.query('account', { filter: { user: { id:currentUser} } });
-    // console.log(accountID.id);
-    // return this.get('store').findRecord('account', accountID);
     return this.store.query('account', { filter: { user:  currentUser } })
     .then((accounts) => {
         return accounts;
@@ -42,8 +15,10 @@ export default Ember.Route.extend({
     });
   },
 
-  updateAccount(params) {
-    console.log('hits');
-    console.log(params);
+  actions: {
+    updateAccount(params) {
+      console.log('hits');
+      console.log(params);
+    },
   }
 });
--- app/routes/accounts/view-account.js
+++ app/routes/accounts/view-account.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 
   model(params){
+    console.log('hits');
     return this.get('store').findRecord('account', params.account_id);
   }
 });
