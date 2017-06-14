4684f699782da5ad63adca7df6d6be5966c022a5
AlexMollohan <ajmollohan@gmail.com>
2017-06-13 20:41:49 -0400

    Reworked routing to send a user signing up/in to their account page

--- app/routes/create-account.js
+++ app/routes/create-account.js
@@ -25,7 +25,7 @@ export default Ember.Route.extend({
       // let account = this.get('store').createRecord('account', params);
       // console.log('account ', account);
       account.save()
-      .then(() => this.transitionTo('accounts'))
+      .then(() => this.transitionTo('accounts.create-account'))
       .then(() => {
         this.get('flashMessages')
         .success('account created');
