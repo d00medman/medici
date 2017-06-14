9f1f30534e67cc3b4143b53401480572c5e9a702
AlexMollohan <ajmollohan@gmail.com>
2017-06-13 20:29:24 -0400

    Current attempt to implement search is bouncing off data store, but is written

--- app/routes/accounts/search-accounts.js
+++ app/routes/accounts/search-accounts.js
@@ -1,9 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
   actions: {
     searchAccount (params) {
-      console.log(params);
+      let userName = params.userName;
+      console.log(userName);
+      this.store.find('account', { user_name: userName })
+      .then((account) => {
+        console.log(account);
+        this.get('flashMessages')
+        .success('account retrieved');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('There was a problem. Please try again.');
+      });
     }
   }
 });
