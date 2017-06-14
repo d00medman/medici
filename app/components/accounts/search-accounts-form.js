8d909b8109201299ebb9864438efc30e1b60512a
AlexMollohan <ajmollohan@gmail.com>
2017-06-13 20:07:10 -0400

    Able to hit appropriate method in route, next step is to hit datastore

--- app/components/accounts/search-accounts-form.js
+++ app/components/accounts/search-accounts-form.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  tagName: 'form',
+  classNames: ['form-horizontal'],
+
+  params : {},
+
+  actions:{
+    submit () {
+      // console.log(this.get('params'))
+      this.sendAction('submit', this.get('params'));
+    },
+
+    reset () {
+      this.set('params', {});
+    },
+  }
 });
