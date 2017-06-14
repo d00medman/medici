f9336d1113370585e1c2dbb3f623549640e9175e
AlexMollohan <ajmollohan@gmail.com>
2017-06-14 09:55:38 -0400

    Able to successfully narrow my search from the local store
    Now that I can return an account, the next step is to redirect the user
    to the url with the account I have successfully extracted.

--- app/routes/accounts/search-accounts.js
+++ app/routes/accounts/search-accounts.js
@@ -4,18 +4,35 @@ export default Ember.Route.extend({
   flashMessages: Ember.inject.service(),
   actions: {
     searchAccount (params) {
-      let userName = params.userName;
-      console.log(userName);
-      this.store.find('account', { user_name: userName })
-      .then((account) => {
-        console.log(account);
-        this.get('flashMessages')
-        .success('account retrieved');
-      })
-      .catch(() => {
-        this.get('flashMessages')
-        .danger('There was a problem. Please try again.');
+
+      this.get('store').findAll('account',{reload:true}).then((accounts) =>{
+        if(accounts.findBy('user_name', params.userName)){
+          let account = accounts.findBy('user_name', params.userName);
+          return account.id;
+        } else {
+          this.get('flashMessages')
+            .danger(`There is no account with the username ${params.userName}`);
+        }
       });
+      // this.store.query('account', { filter: { user_name: params.userName } })
+      //   .then(accounts =>{
+      //     let targ = accounts.get('firstObject').get('user_name') === params.userName;
+      //     console.log(targ);
+      // });
+
+      // this.get('store').peekAll('account')
+      // .then((accounts) => {
+      //   return accounts.filterBy('user_name', params.userName);
+      // })
+      // .then((account) => {
+      //   console.log(account);
+      //   this.get('flashMessages')
+      //   .success('account retrieved');
+      // })
+      // .catch(() => {
+      //   this.get('flashMessages')
+      //   .danger('There was a problem. Please try again.');
+      // });
     }
   }
 });
