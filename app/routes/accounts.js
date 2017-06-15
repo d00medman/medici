0eaf866342cc51457a60dd27f39355f92d940d21
AlexMollohan <ajmollohan@gmail.com>
2017-06-15 11:11:24 -0400

    my-account component refactored; still trying to figure out how to inject isAuthenticated property

--- app/routes/accounts.js
+++ app/routes/accounts.js
@@ -8,6 +8,8 @@ export default Ember.Route.extend({
   //   };
   // },
   auth: Ember.inject.service(),
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
 
   // model (params) {
     // console.log(params.account_id);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({
+export default Ember.Route.extend({
   auth: Ember.inject.service(),
   user: Ember.computed.alias('auth.credentials.email'),
   isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
-
-  actions:{
-    submit(params){
-      this.sendAction('submit', params);
-    },
-
-    delete(){
-      this.sendAction('delete');
-    }
-  }
 });
